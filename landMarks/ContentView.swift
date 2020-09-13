dea5a225aa70398c0ac783494f2c6e802c0ed9ba
Ivan Asonov <asonovivan88@gmail.com>
2020-09-13 21:48:19 +0200

    Initial Commit

---
+++ landMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  landMarks
+//
+//  Created by Ivan Asonov on 13/09/2020.
+//  Copyright © 2020 swiftUITutorial. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
