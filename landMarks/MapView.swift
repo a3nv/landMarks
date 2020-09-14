d70df02058288f86979915042c10fe7cd52b85b5
Ivan Asonov <asonovivan88@gmail.com>
2020-09-14 08:11:20 +0200

    Creating and combining views

---
+++ landMarks/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  landMarks
+//
+//  Created by Ivan Asonov on 13/09/2020.
+//  Copyright © 2020 swiftUITutorial. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordiante = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordiante, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
